723b9e812b0bdf80141c42395cb63b97555b5df8
Pavel Zhuchkov <paz030299@gmail.com>
2021-07-23 12:39:55 +0300

    Added HeaderInterceptor

--- app/src/main/java/com/example/pixabayapp/AppComponent.kt
+++ app/src/main/java/com/example/pixabayapp/AppComponent.kt
@@ -10,7 +10,6 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Singleton
 
 @Component(modules = [AppModule::class])
 interface AppComponent {
@@ -22,9 +21,13 @@ object AppModule {
 
     @Provides
     fun providePixabayService(): PixabayService {
-        val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor()
-        interceptor.level = HttpLoggingInterceptor.Level.BODY
-        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        val headerInterceptor = HeaderInterceptor()
+        val client = OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(headerInterceptor)
+            .build()
 
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.pexels.com")
